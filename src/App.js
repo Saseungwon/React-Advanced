a728a27dc0717cc028809719ee4f11de0935b82d
dfg1425@naver.com <dfg1425@naver.com>
2022-06-13 00:03:16 +0900

    fowordRef

--- src/App.js
+++ src/App.js
@@ -1,46 +1,94 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React from "react";
+import React, { useRef, useState } from "react";
+import Modal from "./components/Modal";
+import MyInput from "./components/MyInput";
 
-const Person = React.memo(({ name, age }) => {
-  console.log("Person render");
-  return (
-    <div>
-      {name} / {age}
-    </div>
-  );
-});
+// const Person = React.memo(({ name, age }) => {
+//   console.log("Person render");
+//   return (
+//     <div>
+//       {name} / {age}
+//     </div>
+//   );
+// });
+
+// function App() {
+//   const [state, setState] = React.useState({
+//     text: "",
+//     persons: [
+//       { id: 1, name: "Mark", age: 36 },
+//       { id: 2, name: "Seungwon", age: 27 },
+//     ],
+//   });
+
+//   const toPersonClick = React.useCallback(() => {}, []);
+
+//   const { text, persons } = state;
+//   return (
+//     <div>
+//       <input type="text" value={text} onChange={change} />
+//       <ul>
+//         {persons.map((person) => {
+//           return <Person {...person} key={person.id} onClick={toPersonClick} />;
+//         })}
+//       </ul>
+//     </div>
+//   );
+// }
+// function change(e) {
+//   setState({
+//     ...state,
+//     text: e.target.value,
+//   });
+// }
+
+// function toPersonClick() {}
+
+//
+// function App() {
+//   const [visible, setVisible] = useState(false); // 초기값 false 지정
+
+//   const open = () => {
+//     setVisible(true);
+//   };
+
+//   const close = () => {
+//     setVisible(false);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={open}>open</button>
+//       {visible && (
+//         <Modal>
+//           <div
+//             style={{
+//               width: "100vw",
+//               height: "100vh",
+//               background: "rgba(0,0,0,0.5)",
+//             }}
+//           ></div>
+//           Hello
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 function App() {
-  const [state, setState] = React.useState({
-    text: "",
-    persons: [
-      { id: 1, name: "Mark", age: 36 },
-      { id: 2, name: "Seungwon", age: 27 },
-    ],
-  });
+  const myInputRef = useRef();
 
-  const toPersonClick = React.useCallback(() => {}, []);
+  const click = () => {
+    console.log(myInputRef.current.value);
+  };
 
-  const { text, persons } = state;
   return (
     <div>
-      <input type="text" value={text} onChange={change} />
-      <ul>
-        {persons.map((person) => {
-          return <Person {...person} key={person.id} onClick={toPersonClick} />;
-        })}
-      </ul>
+      <MyInput ref={myInputRef} />
+      <button onClick={click}>send</button>
     </div>
   );
 }
-function change(e) {
-  setState({
-    ...state,
-    text: e.target.value,
-  });
-}
-
-function toPersonClick() {}
 
 export default App;
